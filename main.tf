04d39b0b7074c1b775197ec248c19830a945671e
maxaviberman <101819587+maxaviberman@users.noreply.github.com>
2022-03-17 18:33:09 +0200

    Terraform initial configuration

---
+++ main.tf
@@ -0,0 +1,161 @@
+provider "aws" {
+  region  = "eu-west-1"
+}
+
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "eu-west-1a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "eu-west-1b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "eu-west-1c"
+}
+
+resource "aws_ecr_repository" "mberman_ecr_repo_httpd" {
+  name = "mberman-ecr-repo/httpd"
+}
+
+resource "aws_ecs_cluster" "bluevine_cluster" {
+  name = "bluevine-cluster"
+}
+
+      #"image": "${aws_ecr_repository.mberman_ecr_repo_httpd.repository_url}",
+
+resource "aws_ecs_task_definition" "httpd_task" {
+  family                   = "httpd_task"
+  container_definitions    = <<DEFINITION
+  [
+      {
+         "command": [
+            "/bin/sh -c \"echo '<html> <head> <title>Bluevine</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h1>Bluevine task</h1> <h2>Max Berman</h2> <p>March, 2022</p> </div></body></html>' >  /usr/local/apache2/htdocs/index.html && httpd-foreground\""
+         ],
+         "entryPoint": [
+            "sh",
+            "-c"
+         ],
+         "essential": true,
+         "image": "httpd:2.4",
+         "logConfiguration": {
+            "logDriver": "awslogs",
+            "options": {
+               "awslogs-group" : "/ecs/fargate-task-definition",
+               "awslogs-region": "eu-west-1",
+               "awslogs-stream-prefix": "ecs"
+            }
+         },
+         "name": "httpd_task",
+         "portMappings": [
+            {
+               "containerPort": 80,
+               "hostPort": 80,
+               "protocol": "tcp"
+            }
+         ]
+      }
+  ]
+  DEFINITION
+
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "bluevine-alb"
+  load_balancer_type = "application"
+  subnets = [
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0    # Allowing any incoming port
+    to_port     = 0    # Allowing any outgoing port
+    protocol    = "-1" # Allowing any outgoing protocol
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "http_target_group" {
+  name        = "http-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}" 
+  health_check {
+    matcher = "200,301,302"
+    path = "/"
+  }
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" 
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.http_target_group.arn}" 
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "httpd_service" {
+  name            = "httpd-service"
+  cluster         = aws_ecs_cluster.bluevine_cluster.id
+  task_definition = aws_ecs_task_definition.httpd_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 2
+  
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.http_target_group.arn}" 
+    container_name   = "${aws_ecs_task_definition.httpd_task.family}"
+    container_port   = 80
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+    security_groups  = ["${aws_security_group.load_balancer_security_group.id}"]
+    assign_public_ip = true
+  }
+}
+
