5451934313a4e7a487a92df7f2f1eff9f74084c8
maxaviberman <101819587+maxaviberman@users.noreply.github.com>
2022-03-18 13:10:41 +0200

    Split the logstash to dedicated service/alb

--- main.tf
+++ main.tf
@@ -26,10 +26,30 @@ resource "aws_ecs_cluster" "bluevine_cluster" {
   name = "bluevine-cluster"
 }
 
-      #"image": "${aws_ecr_repository.mberman_ecr_repo_httpd.repository_url}",
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
 
-resource "aws_ecs_task_definition" "httpd_task" {
-  family                   = "httpd_task"
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+###### LOGSTASH ######  
+resource "aws_ecs_task_definition" "logstash_task" {
+  family                   = "logstash_task"
   container_definitions    = <<DEFINITION
   [
       {
@@ -51,7 +71,91 @@ resource "aws_ecs_task_definition" "httpd_task" {
                "protocol": "tcp"
             }
          ]
-      },
+      }
+  ]
+  DEFINITION
+
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 2048
+  cpu                      = 256
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_ecs_service" "logstash_service" {
+  name            = "logstash-service"
+  cluster         = aws_ecs_cluster.bluevine_cluster.id
+  task_definition = aws_ecs_task_definition.logstash_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 2
+  
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.logstash_target_group.arn}" 
+    container_name   = "${aws_ecs_task_definition.logstash_task.family}"
+    container_port   = 8090
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+    security_groups  = ["${aws_security_group.load_balancer_security_group.id}","${aws_security_group.logstash_security_group.id}"]
+    assign_public_ip = true
+  }
+}
+
+resource "aws_security_group" "logstash_security_group" {
+  ingress {
+    from_port   = 8090
+    to_port     = 8090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0    # Allowing any incoming port
+    to_port     = 0    # Allowing any outgoing port
+    protocol    = "-1" # Allowing any outgoing protocol
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_alb" "logstash_load_balancer" {
+  name               = "logstash-alb"
+  load_balancer_type = "application"
+  subnets = [
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+  security_groups = ["${aws_security_group.logstash_security_group.id}"]
+}
+
+resource "aws_lb_target_group" "logstash_target_group" {
+  name        = "logstash-target-group"
+  port        = 8090
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}" 
+  health_check {
+    matcher = "200,301,302"
+    path = "/"
+  }
+}
+
+resource "aws_lb_listener" "logstash_listener" {
+  load_balancer_arn = "${aws_alb.logstash_load_balancer.arn}" 
+  port              = "8090"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.logstash_target_group.arn}" 
+  }
+}
+
+###### HTTP ######
+resource "aws_ecs_task_definition" "httpd_task" {
+  family                   = "httpd_task"
+  container_definitions    = <<DEFINITION
+  [
       {
          "essential":true,
          "image":"906394416424.dkr.ecr.us-east-1.amazonaws.com/aws-for-fluent-bit:stable",
@@ -83,7 +187,7 @@ resource "aws_ecs_task_definition" "httpd_task" {
             "logDriver": "awsfirelens",
                "options": {
                 "Name": "http",
-                "Host": "127.0.0.1",
+                "Host": aws_alb.logstash_load_balancer.dns_name,
                 "Port": "8090",
                 "Format": "json"
             }
@@ -102,36 +206,9 @@ resource "aws_ecs_task_definition" "httpd_task" {
 
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  memory                   = 2048
+  memory                   = 512
   cpu                      = 256
   execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
-}
-
-resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-}
-
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_alb" "application_load_balancer" {
-  name               = "bluevine-alb"
-  load_balancer_type = "application"
-  subnets = [
-    "${aws_default_subnet.default_subnet_a.id}",
-    "${aws_default_subnet.default_subnet_b.id}",
-    "${aws_default_subnet.default_subnet_c.id}"
-  ]
-  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
 }
 
 resource "aws_security_group" "load_balancer_security_group" {
@@ -150,20 +227,15 @@ resource "aws_security_group" "load_balancer_security_group" {
   }
 }
 
-resource "aws_security_group" "logstash_security_group" {
-  ingress {
-    from_port   = 8090
-    to_port     = 8090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0    # Allowing any incoming port
-    to_port     = 0    # Allowing any outgoing port
-    protocol    = "-1" # Allowing any outgoing protocol
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_alb" "application_load_balancer" {
+  name               = "bluevine-alb"
+  load_balancer_type = "application"
+  subnets = [
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
 }
 
 resource "aws_lb_target_group" "http_target_group" {
@@ -178,21 +250,6 @@ resource "aws_lb_target_group" "http_target_group" {
   }
 }
 
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" 
-  port              = "80"
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.http_target_group.arn}" 
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = aws_iam_role.ecsTaskExecutionRole.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 resource "aws_ecs_service" "httpd_service" {
   name            = "httpd-service"
   cluster         = aws_ecs_cluster.bluevine_cluster.id
@@ -205,14 +262,19 @@ resource "aws_ecs_service" "httpd_service" {
     container_name   = "${aws_ecs_task_definition.httpd_task.family}"
     container_port   = 80
   }
+}
 
-  network_configuration {
-    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
-    security_groups  = ["${aws_security_group.load_balancer_security_group.id}","${aws_security_group.logstash_security_group.id}"]
-    assign_public_ip = true
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" 
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.http_target_group.arn}" 
   }
 }
 
+###### Route53 ######
 resource "aws_route53_record" "www" {
   zone_id = "Z03953912FAQYX9QC55AM"
   name    = "www.mberman.co.uk"
@@ -224,4 +286,3 @@ resource "aws_route53_record" "www" {
     evaluate_target_health = true
   }
 }
-
